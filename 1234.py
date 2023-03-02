173f9c5742add6feddddc7a7461fb8383227499f
Muskan Guptaa <muskan.gupta20@st.niituniversity.in>
2023-03-02 15:40:21 +0530

    branching

--- 1234.py
+++ 1234.py
@@ -3,4 +3,7 @@ It is most suitable when there are multiple people working on projects as a team
 With the help of the versioning system, the developer can identify who has made what changes and then run tests and fix bugs if any and then do necessary feature implementation.
 In case of any unforeseen circumstances, the code can be reverted to any of the previously working versions thereby saving huge efforts.
 
+#trying branching in git
 
+Due to a well-established version control system and the support for collaborative work, git has garnered wide popularity not just amongst the software developers, but also among the people who do other tasks like documentation or 
+any other collaborative work. It can seem challenging at first, but once we get the hang of git, we find that it makes our lives much simpler.
